8bf91c3b4a0a2442bfeb166900308823a56ebbe7
TheK4n <djvlad967891@gmail.com>
2022-02-06 03:24:23 +0300

    add transaction update sellers balance

--- transactions/buy.sql
+++ transactions/buy.sql
@@ -5,6 +5,9 @@ BEGIN;
     -- minus item price from customer balance
     UPDATE customers SET balance = balance - (SELECT price FROM items WHERE pid = 3) WHERE pid = 2;
 
+    -- add item price to seller balance if item belongs to seller
+    UPDATE sellers SET balance = balance + (SELECT price FROM items WHERE pid = 3) WHERE pid = (SELECT seller_pid FROM items WHERE pid = 3);
+
     -- minus one of amount item
     UPDATE items SET amount = amount - 1 WHERE pid = 3;
 COMMIT;
