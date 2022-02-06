b1c44e511ab62243b3ca466375ef9cc27a47c568
TheK4n <djvlad967891@gmail.com>
2022-02-06 03:23:40 +0300

    fix type balance to customers and sellers tables

--- tables/2_sellers.sql
+++ tables/2_sellers.sql
@@ -3,5 +3,6 @@ CREATE TABLE sellers(
     uid UUID NOT NULL DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL CHECK(length(name) <= 70),
     email TEXT NOT NULL UNIQUE CHECK(length(email) <= 100),
+    balance NUMERIC(10,2) NOT NULL DEFAULT 0 CHECK(balance >= 0),
     reg DATE NOT NULL DEFAULT now()
 );
