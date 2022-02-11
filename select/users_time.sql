57ce533920267e72e8ec4f6208917fe99cbeaf0f
TheK4n <djvlad967891@gmail.com>
2022-02-11 21:55:20 +0300

    add select query

--- select/users_time.sql
+++ select/users_time.sql
@@ -15,4 +15,5 @@ SELECT * FROM sellers WHERE pid = (SELECT seller_pid FROM items WHERE pid = (SEL
 -- Топ продавцов по сумме продаж
 SELECT s.name, sum(i.price) AS order_sums FROM orders AS o INNER JOIN items AS i ON o.item_pid = i.pid INNER JOIN sellers AS s ON i.seller_pid = s.pid GROUP BY s.name, s.uid ORDER BY sum(i.price) DESC;
 
-
+-- Колличество покупок товаров у конкретного продавца конкретным покупателем
+SELECT u.uid, s.uid, count(1) as interactions from customers as u inner join orders as o on u.pid = o.customer_pid inner join items as i on o.item_pid = i.pid inner join sellers as s on i.seller_pid = s.pid group by u.uid, s.uid order by interactions desc;
