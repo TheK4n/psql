505615056e762eaa0e75639135326c8c352f3902
TheK4n <djvlad967891@gmail.com>
2022-12-14 20:01:23 +0300

    environs

--- populate_all.py
+++ populate_all.py
@@ -2,10 +2,18 @@ from db import ConnectionData, PostgreSQL
 from populate_custs import populate_customers
 from populate_itm import populate_items_for_many_sellers
 from populate_sel import populate_sellers
+import os
+
 
 if __name__ == '__main__':
 
-    conn_data = ConnectionData(host="127.0.0.1", port=5432, dbname="shop", user="shop_admin", password="1234")
+    host = os.environ.get("PG_HOST", "127.0.0.1")
+    port = int(os.environ.get("PG_PORT", 5432))
+    dbname = os.environ.get("PG_DB", "postgres")
+    user = os.environ.get("PG_USER", "postgres")
+    password = os.environ.get("PG_PASSWORD", "1234")
+
+    conn_data = ConnectionData(host=host, port=port, dbname=dbname, user=user, password=password)
     db = PostgreSQL(conn_data)
     populate_customers(db)
     populate_sellers(db)
